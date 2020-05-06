0ac97149c9048cb80f4ccdb7afa14e63c738dc56
Lucas Resa <lukkitas.1597@gmail.com>
2020-05-06 05:12:11 -0300

    Anulo el moviemiento en la camara ya que el cuerpo rigido tomara esa responsabilidad, el rigido del personaje ahora es una capsula y la camara se situa en su parte superior (esto se puede ajustar) y retoco un poco el moviemiento del personaje para que quede mas realista

--- TGC.Group/Model/Bullet/Bodies/CharacterRigidBody.cs
+++ TGC.Group/Model/Bullet/Bodies/CharacterRigidBody.cs
@@ -1,6 +1,8 @@
 ﻿using BulletSharp;
 using BulletSharp.Math;
 using Microsoft.DirectX.DirectInput;
+using System;
+using TGC.Core.BoundingVolumes;
 using TGC.Core.Input;
 using TGC.Core.Mathematica;
 using TGC.Group.Utils;
@@ -9,9 +11,16 @@ namespace TGC.Group.Model.Bullet.Bodies
 {
     class CharacterRigidBody : RigidBody
     {
+        struct Constants
+        {
+            public static float speed = 450f;
+            public static TGCVector3 cameraHeight = new TGCVector3(0, 85, 0);
+            public static float capsuleSize = 160f;
+            public static float capsuleRadius = 40f;
+
+        }
+
         private CameraFPS Camera;
-        private TGCVector3 directorz = new TGCVector3(1, 0, 0);
-        private TGCVector3 directorx = new TGCVector3(0, 0, 1);
         private TGCVector3 position;
         private TGCVector3 indoorPosition;
         private TGCVector3 outdoorPosition;
@@ -29,67 +38,84 @@ namespace TGC.Group.Model.Bullet.Bodies
             else position = Camera.getIndoorPosition();
 
             #region Create rigidBody
-            body = rigidBodyFactory.CreateBall(30f, 0.75f, position);
+            body = rigidBodyFactory.CreateCapsule(Constants.capsuleRadius, Constants.capsuleSize, position, 1f, false);
             body.CenterOfMassTransform = TGCMatrix.Translation(position).ToBulletMatrix();
             #endregion
         }
 
         public override void Update(TgcD3dInput input, float elapsedTime)
         {
-            var strength = 5f;
+            var speed = Constants.speed;
             body.ActivationState = ActivationState.ActiveTag;
-        
+
             #region Movimiento 
+
             body.AngularVelocity = TGCVector3.Empty.ToBulletVector3();
 
             var director = Camera.LookAt - Camera.position;
             director.Normalize();
 
-            if (input.keyDown(Key.R))
+            if (!isOutOfWater())
+            {
+                if (input.keyDown(Key.W))
+                {
+                    body.LinearVelocity = director.ToBulletVector3() * speed;
+                }
+
+                if (input.keyDown(Key.S))
+                {
+                    body.LinearVelocity = director.ToBulletVector3() * -speed;
+                }
+
+                if (input.keyDown(Key.A))
+                {
+                    director.TransformCoordinate(TGCMatrix.RotationY(FastMath.PI_HALF));
+                    body.LinearVelocity = director.ToBulletVector3() * -speed;
+                }
+
+                if (input.keyDown(Key.D))
+                {
+                    director.TransformCoordinate(TGCMatrix.RotationY(FastMath.PI_HALF));
+                    body.LinearVelocity = director.ToBulletVector3() * speed;
+                }
+
+                if (input.keyDown(Key.Space))
+                {
+                    body.LinearVelocity = Vector3.UnitY * speed;
+                }
+
+                if (input.keyDown(Key.LeftControl))
+                {
+                    body.LinearVelocity = Vector3.UnitY * -speed;
+                }
+            }
+            else
+                body.ApplyCentralImpulse(Vector3.UnitY * -5);
+
+            if (input.keyUp(Key.W) || input.keyUp(Key.S) || input.keyUp(Key.A) || input.keyUp(Key.D) 
+                 || input.keyUp(Key.LeftControl) || input.keyUp(Key.Space))
             {
                 body.LinearVelocity = Vector3.Zero;
                 body.AngularVelocity = Vector3.Zero;
             }
 
-            if (input.keyDown(Key.W))
-            {
-                body.ApplyCentralImpulse(strength * director.ToBulletVector3());
-            }
-
-            if (input.keyDown(Key.S))
-            {
-                body.ApplyCentralImpulse(strength * director.ToBulletVector3());
-            }
-
-            if (input.keyDown(Key.A))
-            {
-                body.ApplyCentralImpulse(strength * director.ToBulletVector3());
-            }
-
-            if (input.keyDown(Key.D))
-            {
-                body.ApplyCentralImpulse(strength * director.ToBulletVector3());
-            }
-
-            if (input.keyPressed(Key.Space))
-            {
-                body.ApplyCentralImpulse(new TGCVector3(0, 80 * strength, 0).ToBulletVector3());
-            }
-
-            if (input.keyPressed(Key.LeftControl))
-            {
-                body.ApplyCentralImpulse(new TGCVector3(0, 80 * -strength, 0).ToBulletVector3());
-            }
-
-            #endregion
-
-            Camera.position = new TGCVector3(body.CenterOfMassPosition);
-
-            #region Teclas
-
             if (input.keyPressed(Key.E)) Teleport();
 
+            body.LinearVelocity += TGCVector3.Up.ToBulletVector3() * getGravity();
+
+            Camera.position = new TGCVector3(body.CenterOfMassPosition) + Constants.cameraHeight;
+
             #endregion
+        }
+
+        private float getGravity()
+        {
+            return body.CenterOfMassPosition.Y < 0 ? -200 : -5;
+        }
+
+        private bool isOutOfWater()
+        {
+            return Camera.position.Y > 3505;
         }
 
         public override void Dispose()
